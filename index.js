ef265df38c0916e16c9bc7cc7f5463b685330adf
pcxpert26 <pcxpert26@gmail.com>
2024-09-13 12:26:02 -0400

    Dealer added into game

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ let player = {
 
 let cards = []
 let sum = 0
+//initialize array and sum for dealer
+let dealerCards = []
+let dealerSum = 0
+//
 let hasBlackJack = false
 let isAlive = false
 let message = ""
@@ -25,14 +29,82 @@ function getRandomCard() {
         return randomNumber
     }
 }
-
+//call dealer function from within startGame
+//hasBlackJack value must reset to false with new game
 function startGame() {
     isAlive = true
+    hasBlackJack = false
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard
+    getDealerHand()
     renderGame()
+}
+
+function getDealerHand() {
+    //assign dealer card values
+    let firstDealerCard = getRandomCard()
+    let secondDealerCard = getRandomCard()
+    //assign array and sum for dealer
+    dealerCards = [firstDealerCard, secondDealerCard]
+    dealerSum = firstDealerCard + secondDealerCard
+    //check for blackjack
+    if (dealerSum === 21) {
+        message = "House wins!"
+        messageEl.textContent = message
+        isAlive = false
+    }
+    //wait for player turn to end then run logic
+    //player selects hold which calls analyzeHand()
+    //analyzeHand() uses rules to determine hit or hold for dealer
+    //16 or less gets hit and 17+ gets hold
+}
+
+function analyzeHand() {
+    //if player has no cards then game has not started and code should not run
+    //if player has blackjack then game should not continue and this code should not run
+    //either game not started OR already won should break out
+    if (isAlive === false || hasBlackJack === true) {
+        return
+    }
+    else if (dealerSum === 21) {
+        message = "House wins!"
+        messageEl.textContent = message
+        isAlive = false}
+    else if (dealerSum < 17) {
+        //call newDealerCard() which puts new card into dealerCards array
+        newDealerCard()
+    } else {
+        //dealerSum is 17+ but not 21 so dealer holds
+        //call function to compare dealerSum to player sum
+        determineGame()
+    }
+}
+
+function determineGame() {
+    //add dealer sum into message to cross check game result
+    //account for 21+ technically being greater than by using &&
+    if (dealerSum > sum && dealerSum <= 21){
+        message = "House wins!" + " Dealer Sum: " + dealerSum
+        messageEl.textContent = message
+        isAlive = false
+    } else if (dealerSum === sum) {
+        message = "The game has ended in a draw!" + " Dealer Sum: " + dealerSum
+        messageEl.textContent = message
+        isAlive = false
+    } else {
+        message = "You win!" + " Dealer Sum: " + dealerSum
+        messageEl.textContent = message
+        isAlive = false
+    }
+}
+
+function newDealerCard() {
+    let dealerCard = getRandomCard()
+        dealerSum += dealerCard
+        dealerCards.push(dealerCard)
+        analyzeHand()
 }
 
 function renderGame() {
